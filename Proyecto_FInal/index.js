9806c788bae260e1d23a74322317427e99987801
DavidFelipeAriza04 <davidariza0406@gmail.com>
2023-05-31 01:57:57 -0500

    Proyecto Final v2

--- Proyecto_FInal/index.js
+++ Proyecto_FInal/index.js
@@ -1,1 +1,21 @@
 AOS.init();
+
+const menu = document.getElementById("menu");
+const menu_icon = document.getElementById("nav-bar__menu");
+const menu_links = document.getElementsByClassName("menu__link");
+
+menu_icon.addEventListener("click",()=>{
+    menu.classList.toggle("hidden");
+    menu.classList.toggle("flex");
+});
+
+console.log(menu_links)
+console.log(menu_links.length);
+
+for(let i=0;i<menu_links.length;i++){
+    menu_links[i].addEventListener("click",()=>{
+        console.log("hola");
+        menu.classList.toggle("hidden");
+        menu.classList.toggle("flex");
+    })
+};
